353a9b780e2accf487985defbdf49faec7a7bace
Emilse Arias <emilsearias@gmail.com>
2022-06-20 20:42:52 -0300

    al comenzar el juego el boton se deshabilita y una vez adivinado el numero indica si quiere volver a comenzar el juego

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 // Crea una web con bootstrap y js, que contenga un botón comenzar el juego, en ese momento se crea un número aleatorio que el usuario deberá adivinar, la interfaz del usuario debe tener además un input para ingresar un número y un botón enviar, al presionar el botón enviar mostrar en un alert si el usuario adivino o no el número mágico, si no lo adivino indicarle con un alert si el numero que ingreso es mayor o menor al número mágico.
 // Cuando el usuario adivine el numero mostrar un mensaje indicando al usuario que adivino el numero.
 let numeroMagico;
+let btnComenzar = document.querySelector("#btnComenzar");
 
 function comenzarJuego() {
   numeroMagico = getRandomArbitrary(1, 101);
   console.log(numeroMagico);
   //   cambio el msj del boton
-  let btnComenzar = document.querySelector("#btnComenzar");
   btnComenzar.className = "btn btn-secondary btn-lg";
   btnComenzar.innerHTML = "El juego comenzo";
+  btnComenzar.disabled = true
 }
 
 // Retorna un número aleatorio entre min (incluido) y max (excluido)
@@ -22,6 +23,9 @@ function consultarNumero() {
   if (numeroIngresado == numeroMagico) {
     mensaje.className = "alert alert-primary";
     mensaje.innerHTML = "Adivinaste 🤩";
+    btnComenzar.className = "btn btn-primary btn-lg";
+    btnComenzar.innerHTML = "Vuelve a jugar";
+    btnComenzar.disabled = false;
   } else if (numeroIngresado > numeroMagico) {
     mensaje.className = "alert alert-info";
     mensaje.innerHTML =
